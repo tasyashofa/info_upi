9a3c2df7692c596f8300fec8a073517340959766
Tasya <tasya.faziyah01@gmail.com>
2022-05-20 17:38:05 +0700

    update fasilitas dan rincianfakultas

--- lib/Fasilitas.dart
+++ lib/Fasilitas.dart
@@ -8,75 +8,74 @@ class Fasilitas extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
-        InkWell(
-          child: Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              //child: Text("baris 1"),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(children: const [
-                    Text(
-                      "KOLAM RENANG",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Fakultas Pendidikan Matematika dan Pengetahuan Alam",
-                      //textAlign: TextAlign.center,
-                    ),
-                  ]),
-                  Column(children: [
-                    Image.network(
-                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-                      width: 50,
-                      height: 50,
-                    ),
-                  ]),
-                ],
-              )),
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-              return RincianFakultas();
-            }));
-            //gunakan navigator untuk panggil RincianFakultas
-          },
-        ),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            //child: Text("baris 1"),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(children: const [
+                  Text(
+                    "KOLAM RENANG",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]),
+                Column(children: [
+                  Image.network(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                    width: 50,
+                    height: 50,
+                  ),
+                ]),
+              ],
+            )),
         Padding(padding: EdgeInsets.all(10)),
-        InkWell(
-          child: Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              //child: Text("baris 1"),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(children: const [
-                    Text(
-                      "GYMNASIUM",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Fakultas Pendidikan Ilmu Pengetahuan Sosial",
-                      //textAlign: TextAlign.center,
-                    ),
-                  ]),
-                  Column(children: [
-                    Image.network(
-                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-                      width: 50,
-                      height: 50,
-                    ),
-                  ]),
-                ],
-              )),
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-              return RincianFakultas();
-            }));
-            //gunakan navigator untuk panggil RincianFakultas
-          },
-        ),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            //child: Text("baris 1"),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(children: const [
+                  Text(
+                    "GYMNASIUM",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]),
+                Column(children: [
+                  Image.network(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                    width: 50,
+                    height: 50,
+                  ),
+                ]),
+              ],
+            )),
+        Padding(padding: EdgeInsets.all(10)),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            //child: Text("baris 1"),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(children: const [
+                  Text(
+                    "MUSEUM",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]),
+                Column(children: [
+                  Image.network(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                    width: 50,
+                    height: 50,
+                  ),
+                ]),
+              ],
+            )),
       ]),
     );
   }
--- lib/RincianFakultas.dart
+++ lib/RincianFakultas.dart
@@ -18,11 +18,71 @@ class RincianFakultas extends StatelessWidget {
                   'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
                   width: 70,
                 )),
-            Column(children: const [
-              Text("FPMIPA"),
-              Text(
-                  "FPMIPA merupakan salah satu fakultas yang terdapat di Universitas Pendidikan Indonesia")
-            ]),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "FPMIPA",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Padding(padding: EdgeInsets.all(5)),
+                  Text(
+                    "FPMIPA merupakan salah satu fakultas yang terdapat di Universitas Pendidikan Indonesia, Terdapat beberapa departemen di FPMIPA diantaranya yaitu :",
+                    style: TextStyle(
+                      wordSpacing: 3,
+                    ),
+                  ),
+                ]),
+            Container(
+                padding: EdgeInsets.all(5),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(border: Border.all()),
+                        width: 300,
+                        height: 40,
+                        padding: EdgeInsets.all(10),
+                        child: Text("Departemen Pendidikan Ilmu Komputer",
+                            textAlign: TextAlign.center),
+                      ),
+                      Padding(padding: EdgeInsets.all(10)),
+                      Container(
+                        decoration: BoxDecoration(border: Border.all()),
+                        width: 300,
+                        height: 40,
+                        padding: EdgeInsets.all(10),
+                        child: Text("Departemen Pendidikan Matematika",
+                            textAlign: TextAlign.center),
+                      ),
+                      Padding(padding: EdgeInsets.all(10)),
+                      Container(
+                        decoration: BoxDecoration(border: Border.all()),
+                        width: 300,
+                        height: 40,
+                        padding: EdgeInsets.all(10),
+                        child: Text("Departemen Pendidikan Biologi",
+                            textAlign: TextAlign.center),
+                      ),
+                      Padding(padding: EdgeInsets.all(10)),
+                      Container(
+                        decoration: BoxDecoration(border: Border.all()),
+                        width: 300,
+                        height: 40,
+                        padding: EdgeInsets.all(10),
+                        child: Text("Departemen Pendidikan Fisika",
+                            textAlign: TextAlign.center),
+                      ),
+                      Padding(padding: EdgeInsets.all(10)),
+                      Container(
+                        decoration: BoxDecoration(border: Border.all()),
+                        width: 300,
+                        height: 40,
+                        padding: EdgeInsets.all(10),
+                        child: Text("Departemen Pendidikan Kimia",
+                            textAlign: TextAlign.center),
+                      ),
+                    ])),
           ])),
     );
   }
